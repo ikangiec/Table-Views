c595d027e128396a76a007f4b03c9016e268a6d0
Kangie Chan <aries0420@gmail.com>
2015-01-11 19:47:51 -0800

    Simple table views app

--- Table Views/ViewController.swift
+++ Table Views/ViewController.swift
@@ -8,7 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    let array = ["Sherman", "Kangie", "Fern", "Kiki"]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        
+        cell.textLabel?.text = array[indexPath.row]
+        
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
